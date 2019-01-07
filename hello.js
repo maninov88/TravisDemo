156abdaeafbafd486e1524618eb9886c6cd0dbf5
maninov88 <maninov88@gmail.com>
2019-01-07 20:50:57 +0530

    Code changed 1

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis!\n'); // build should pass now!
+  res.end('Hello Travis - modified by master!\n'); // build should pass now!
 }).listen(1337, '127.0.0.1');
 //Comment added by release-1
 //comment added by master
